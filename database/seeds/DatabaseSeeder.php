5a5b8872dc7c74857d72d4d3aedc83279a6de0b2
Po Xuan <wdftmmas234@gmail.com>
2019-12-08 19:14:04 +0800

    create food 資料表欄位、DataSeeder中填充food的資料

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('food')->insert([
+            'fdName' => str_random(10),
+            'gram' => (float)rand() / (float)getrandmax(),
+            'calorie' => (float)rand() / (float)getrandmax(),
+            'protein' => (float)rand() / (float)getrandmax(),
+            'fat' => (float)rand() / (float)getrandmax(),
+            'saturatedFat' => (float)rand() / (float)getrandmax(),
+            'transFat' => (float)rand() / (float)getrandmax(),
+            'cholesterol' => (float)rand() / (float)getrandmax(),
+            'carbohydrate' => (float)rand() / (float)getrandmax(),
+            'sugar' => (float)rand() / (float)getrandmax(),
+            'dietaryFiber' => (float)rand() / (float)getrandmax(),
+            'sodium' => (float)rand() / (float)getrandmax(),
+            'calcium' => (float)rand() / (float)getrandmax(),
+            'potassium' => (float)rand() / (float)getrandmax(),
+            'ferrum' => (float)rand() / (float)getrandmax(),
+            'photo' => "http://localhost/qrfood/img/01.jpg",
+            
+
+        ]);
     }
+   
 }
