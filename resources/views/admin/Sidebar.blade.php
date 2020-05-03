01a38bdd2caa0d10f6e49502bcc9bf7433e0b118
rogers1027 <weichundot@gmail.com>
2020-05-03 14:49:36 +0800

    新增index Route、刪除不必要的東西

--- resources/views/admin/Sidebar.blade.php
+++ resources/views/admin/Sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Sidebar -->
 <div class="bg-light border-right" id="sidebar-wrapper">
   <div class="sidebar-heading">QRFood管理平台</div>
-  <div class="list-group list-group-flush">
+  <div class="list-group list-group-flush" >
     <a href="{{ route('manage_food') }}" class="list-group-item list-group-item-action bg-light">食物管理</a>
     {{-- <a href="#" class="list-group-item list-group-item-action bg-light">Restaurant</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
