b8ccc0921dd066a8ebeebec449d91ae6fd621d45
PoXuanla <wdftmmas234@gmail.com.tw>
2020-07-15 13:41:48 +0800

    "單筆新增食物":圖片上傳2mb限制提醒

--- resources/views/admin/food/modal.blade.php
+++ resources/views/admin/food/modal.blade.php
@@ -545,3 +545,19 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="Modal_FailedMessage" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" i>錯誤訊息</h5>
+            </div>
+            <div class="modal-body">
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
